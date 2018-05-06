99f657952d1d20407760618c013da159b4d77705
Mx. Kas Perch <the@nodebotani.st>
2018-05-06 16:15:06 -0500

    Built out j5 sensor

--- ColorSensor.js
+++ ColorSensor.js
@@ -1,3 +1,15 @@
+let priv = new Map()
+
+let controllers = {
+  AS7262: {
+    ADDRESSES: {
+      value: [0x00]
+    },
+    REGISTERS: {},
+    initialize: function() {}
+  }
+}
+
 function ColorSensor(opts) {
   if (!this instanceof ColorSensor) {
     return new ColorSensor(opts)
@@ -38,6 +50,51 @@ function ColorSensor(opts) {
       stash: []
     }
   }
+
+  // Initializes component with j5 (boilerplate for Component objects)
+  Board.Component.call(this, (opts = Board.Options(opts)))
+
+  // select the controller
+  if (opts.controller && typeof opts.controller === 'string') {
+    controller = Controllers[opts.controller.toUpperCase()]
+  } else {
+    controller = opts.controller
+  }
+
+  // if no controller is sent
+  if (controller == null) {
+    controller = Controllers.AS7262
+  }
+
+  // boilerplate for component with a controller object (called AFTER controller is ID'd)
+  Board.Controller.call(this, controller, opts)
+
+  // TODO: ID this code
+  if (!this.enabledChanged) {
+    this.enabledChanged = function() {}
+  }
+
+  // set the state map
+  priv.set(this, state)
+
+  // TODO: does initialize only run at start, or every data acquisition?
+  if (typeof this.initialize === 'function') {
+    this.initialize(opts, function(data) {
+      var isChange = false
+
+      if (!state.enabled) {
+        return
+      }
+
+      this.emit('data', {})
+
+      if (isChange) {
+        this.emit('change', {})
+      }
+    })
+  }
 }
+
+util.inherits(ColorSensor, Emitter)
 
 module.exports = ColorSensor
